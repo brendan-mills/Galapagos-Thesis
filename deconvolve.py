f906528ff3f09cffb0fc222b97a0b4b94b84fdec
Brendan Mills <brendanrmills@gmail.com>
2022-10-20 13:18:33 -0700

    took out some imports

--- deconvolve.py
+++ deconvolve.py
@@ -5,13 +5,10 @@ Created on Wed Oct  5 13:05:49 2022
 
 @author: brendanmills
 """
-import numpy as np
-import matplotlib.pyplot as plt
 import obspy as opy
 from obspy import UTCDateTime
 from obspy.clients.fdsn import Client
 import time
-import os
 
 start_timer = time.time()
 t0 = UTCDateTime("2018-06-26T0:0:00.000")
@@ -23,7 +20,6 @@ stasel="SN11" # station code selection (* for all)
 year = 2018
 info = '{}.{}..{}.{}'.format(netsel, stasel, chnsel, year)
 client = Client("IRIS")
-
 day = 1
 
 def create_padded_stream(start_day, end_day, padding_hrs=3, info=info):
