18a1966cdcb705b6b63fce4e97933fdd5ef88b91
Brendan Mills <brendanrmills@gmail.com>
2022-11-09 12:02:28 -0800

    added more prefiltering

--- decon_bp.py
+++ decon_bp.py
@@ -17,6 +17,7 @@ import obspy as opy
 from obspy import UTCDateTime
 from obspy.clients.fdsn import Client
 import time
+
 #this is for looking at the onset tremor so the days are hardcoded for 177 and the times are relevant to the tremor
 start_timer = time.time()
 t0 = UTCDateTime("2018-06-26T0:0:00.000")
@@ -41,48 +42,64 @@ def gather(sta_list):
         stream.append(st[0])
     return stream
 
-stas = ['SN04','SN05', 'SN07', 'SN11', 'SN12']
+def gather_all():
+    stream = opy.core.stream.Stream()
+    for i in range(17):
+        if i<10:
+            sta = 'SN0'+str(i)
+        else:
+            sta = 'SN'+str(i)
+        info = '8G.{}..{}.{}'.format(sta, chnsel, year)
+        file_name = '{}.177.mseed'.format(info)
+        try:
+            info = '8G.{}..{}.{}'.format(sta, chnsel, year)
+            file_name = '{}.177.mseed'.format(info)
+            st = opy.read('/Volumes/LaCie/SIERRA_NEGRA/SN_GALAPAGOS/'+file_name)
+            stream.append(st[0])
+        except:
+            pass
+        try:
+            info = 'EC.{}..{}.{}'.format(sta, chnsel, year)
+            file_name = '{}.177.mseed'.format(info)
+            st = opy.read('/Volumes/LaCie/SIERRA_NEGRA/SN_EC/'+file_name)
+            stream.append(st[0])
+        except:
+            pass
+    return stream
 
 def deconvolve_prefilt(st):
-    pre_filt = [0.5, 0.7, 9, 10]
+    pre_filt = [0.5, 0.7, 17, 20]
     st_decon = opy.core.stream.Stream()
-    for i in range(len(stas)):
-        inventory = client.get_stations(network=netsel, station=stas[i], channel=chnsel, location='*', starttime=t0, endtime=t0+tdur, level = 'response')
+    for tr in st:
+        stats = tr.stats
+        inventory = client.get_stations(network=stats.network,
+                                        station=stats.station, 
+                                        channel=stats.channel, 
+                                        location='*', 
+                                        starttime=stats.starttime,
+                                        endtime=stats.endtime, 
+                                        level = 'response')
         
-        st_d = st[i].remove_response(inventory=inventory, pre_filt=pre_filt, output='VEL')#removes response
+        st_d = tr.remove_response(inventory=inventory, pre_filt=pre_filt, output='VEL')#removes response
         st_d.detrend('demean')
         st_decon.append(st_d)
-    file_name = '/Volumes/LaCie/SN_Thesis/Day177/8G.Array..HHZ.2018.177.BP1.Decon.mseed'
+        print('Done with ' + stats.station)
+    file_name = '/Volumes/LaCie/SN_Thesis/Day177/8G.Array..HHZ.2018.177.BP3.Decon.mseed'
     print(file_name)
     st_decon.write(file_name)
     return st_decon
 
-def gather_all():
-    stream = opy.core.stream.Stream()
-    for i in range(1,15):
-        if i <10:
-            sta = f'SN0{i+1}'
-        else:
-            sta = f'SN{i+1}'
-        info = '{}.{}..{}.{}'.format(netsel, sta, chnsel, year)
-        file_name = '/Volumes/LaCie/SIERRA_NEGRA/SN_GALAPAGOS/{}.177.mseed'.format(info)
-        try:
-            st = opy.read(file_name)
-            stream.append(st[0])
-        except:
-            print(f'No data for {sta}')
-    return stream
-
-stream = gather(stas)
+stream = opy.read('/Volumes/LaCie/SN_Thesis/Day177/8G.Array..HHZ.2018.177.mseed')
 print(stream)
-stream.plot()
 st_d = deconvolve_prefilt(stream)
 print(st_d)
-st_d.plot()
 end_timer = time.time()
 print('This all took {} seconds'.format( round(end_timer-start_timer,2)) )
 
 # prefilt = [0.0001, 0.0002, samp_rate/2-2, samp]
 # prefilt = [0.5, 0.7, 9, 10]
 # st.remove_response(inventory=inventory, pre_filt= pre_filt, output = 'VEL', waterlevel= None)
-# 
+# pre_filt1 = [0.5, 0.7, 9, 10]
+# pre_filt3 = [0.5, 0.7, 17, 20]
+# pre_filt2 = [0.001, 0.01, 40, 45]
+
