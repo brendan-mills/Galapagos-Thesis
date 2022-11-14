227c297d1736a01921fea052947e3532b6002d4a
Brendan Mills <brendanrmills@gmail.com>
2022-11-14 11:13:36 -0800

    made funtion based bandpass

--- decon_bp.py
+++ decon_bp.py
@@ -67,10 +67,9 @@ def gather_all():
             pass
     return stream
 
-def deconvolve_prefilt(st):
-    pre_filt = [0.5, 0.7, 17, 20]
+def deconvolve_prefilt(st, pf, out_name):
     st_decon = opy.core.stream.Stream()
-    for tr in st:
+    for tr in st.copy():
         stats = tr.stats
         inventory = client.get_stations(network=stats.network,
                                         station=stats.station, 
@@ -80,19 +79,30 @@ def deconvolve_prefilt(st):
                                         endtime=stats.endtime, 
                                         level = 'response')
         
-        st_d = tr.remove_response(inventory=inventory, pre_filt=pre_filt, output='VEL')#removes response
+        st_d = tr.remove_response(inventory=inventory, pre_filt=pf, output='VEL')#removes response
         st_d.detrend('demean')
         st_decon.append(st_d)
         print('Done with ' + stats.station)
-    file_name = '/Volumes/LaCie/SN_Thesis/Day177/8G.Array..HHZ.2018.177.BP3.Decon.mseed'
+    file_name = '/Volumes/LaCie/SN_Thesis/Day177/'+out_name
     print(file_name)
     st_decon.write(file_name)
     return st_decon
 
 stream = opy.read('/Volumes/LaCie/SN_Thesis/Day177/8G.Array..HHZ.2018.177.mseed')
-print(stream)
-st_d = deconvolve_prefilt(stream)
-print(st_d)
+files = ['8G.Array..HHZ.2018.177.BP1.mseed', 
+         '8G.Array..HHZ.2018.177.BP2.mseed',
+         '8G.Array..HHZ.2018.177.BP3.mseed',
+         '8G.Array..HHZ.2018.177.BP4.mseed']
+filters = [ [0.5, 0.7, 9, 10], 
+            [0.5, 0.7, 17, 20],
+            [0.001, 0.01, 17, 20],
+            [0.001, 0.01, 40, 45]] 
+
+stream = opy.read('/Volumes/LaCie/SN_Thesis/Day177/8G.Array..HHZ.2018.177.mseed')
+for i in range(4):
+    print('Start '+files[i])
+    st_d = deconvolve_prefilt(stream, filters[i], files[i])
+    
 end_timer = time.time()
 print('This all took {} seconds'.format( round(end_timer-start_timer,2)) )
 
@@ -100,6 +110,6 @@ print('This all took {} seconds'.format( round(end_timer-start_timer,2)) )
 # prefilt = [0.5, 0.7, 9, 10]
 # st.remove_response(inventory=inventory, pre_filt= pre_filt, output = 'VEL', waterlevel= None)
 # pre_filt1 = [0.5, 0.7, 9, 10]
-# pre_filt3 = [0.5, 0.7, 17, 20]
-# pre_filt2 = [0.001, 0.01, 40, 45]
+# pre_filt2 = [0.5, 0.7, 17, 20]
+# pre_filt3 = [0.001, 0.01, 40, 45]
 
